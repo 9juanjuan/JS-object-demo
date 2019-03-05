00bf82168518cfa6e22e5ed575e0e25dfbad63c9
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-03-05 11:19:47 -0500

    moves l33t dictionary into the translate function

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,32 @@
-const leetDictionary = {
-A : 4,
-E : 3,
-G : 6,
-I : 1,
-O : 0,
-S : 5,
-T : 7
-};
+
+console.log('Hello');
+
 
 function transl33t(originalString) {
+    const leetDictionary = {
+        A : "4",
+        E : "3",
+        G : "6",
+        I : "1",
+        O : "0",
+        S : "5",
+        T : "7"
+        };
     let result = '';
-
-
-
+// loop through each letter in originalString
+//  if its in the dictionary, and if it is
+//  put its value in 'result
+//  if its not, just copy the letter into 'result
+for(let i = 0; i< originalString.length; i++){
+    let theLetter = originalString[i].toUpperCase();
+    // console.log(theLetter);
+    // console.log(leetDictionary[theLetter]);
+        if (leetDictionary[theLetter]){
+        result += leetDictionary[theLetter]
+        } else {
+         result += theLetter
+        }
+    
+    }
     return result;
 }
